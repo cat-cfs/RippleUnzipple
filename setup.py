5a95a8a5c91d1167e6d93b7d1f36800d8b8b1c47
Anthony Rodway <anthony.rodway@nrcan-rncan.gc.ca>
2024-01-08 08:22:01 -0800

    Update version for package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RippleUnzipple',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'py7zr==0.20.8',
